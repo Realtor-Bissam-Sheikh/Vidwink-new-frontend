85e00cc9c5f89ac763aa7e3b9907b89a54723a43
saidtome97 <128271175+saidtome97@users.noreply.github.com>
2025-05-31 07:50:26 +0400

    Update App.jsx

--- public/src/App.jsx
+++ public/src/App.jsx
@@ -1,3 +1,8 @@
+import { BASE_URL } from './config';
+
+fetch(`${BASE_URL}/ping`)
+  .then(response => response.text())
+  .then(data => console.log(data));
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
